38dd7a3bdaa47e40ca498ee3a1972ec711133f29
Fran Alarza <franalarza@gmail.com>
2022-07-06 15:22:17 +0200

    Initial Commit

---
+++ Dragon BallUITests/Dragon_BallUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dragon_BallUITestsLaunchTests.swift
+//  Dragon BallUITests
+//
+//  Created by Fran Alarza on 6/7/22.
+//
+
+import XCTest
+
+class Dragon_BallUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
